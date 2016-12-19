29bfed841f28484306b654370698e59ab7fd0c35
ercekal <ercekalabalikoglu@gmail.com>
2016-12-19 16:12:10 +0000

    action and reducer is added for authentication

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          Deneme
+        </li>
+        <li>
+          Testing
+        </li>
+      </ul>
+    </div>
+  );
+};
